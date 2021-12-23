692f553cefbc22c82676da87c13760ecf3a1b074
kanonkc <kanonkce@gmail.com>
2021-12-23 21:36:47 +0700

    Robot

---
+++ Solutions-2021/Week 3 - Searching & Sorting/robot2000.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+char Direction[] = {'N','E','S','W'};
+
+string transform(string v){
+    int dir = 0;
+    //  0
+    //3   1
+    //  2
+    for(int i=0;i<v.length();i++){
+        if
+    }
+}
+
+int main(){
+    string v1000 = "NNEESWZEE";
+    transform(v1000);
+}
