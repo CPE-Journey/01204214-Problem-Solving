bdbb1e127f63e4c19bc40309c6e8756e9e3a020d
KanonKC <kanonkce@gmail.com>
2022-01-10 14:12:29 +0700

    1/8

---
+++ Week 5 - Graph Traversal & Greedy/fin62_name.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int current = 0,count = 0,len;
+string name,message;
+
+int main(){
+    cin >> name >> message;
+    len = name.length();
+    for(int i=0;i<message.length();i++){
+        if(message[i] == name[current]){
+            count += (current+1)/len;
+            current = (current+1)%len;
+        }
+    }
+
+    cout << count;
+}
