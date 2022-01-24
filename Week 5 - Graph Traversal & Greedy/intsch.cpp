1c95231b117110a4d8afa1b96b3a01cc99cf608e
KanonKC <kanonkce@gmail.com>
2022-01-24 14:09:31 +0700

    intsch

--- Week 5 - Graph Traversal & Greedy/intsch.cpp
+++ Week 5 - Graph Traversal & Greedy/intsch.cpp
@@ -7,12 +7,6 @@ int N,i,work_count=1;
 long long int current,t1,t2;
 vector<pair<long long int,long long int>> work;
 
-void printVector(vector<pair<long long int,long long int>> v){
-    for(int i=0;i<v.size();i++){
-        cout << v[i].first << " " << v[i].second << "\n";
-    }
-}
-
 int main(){
     cin >> N;
     for(int i=0;i<N;i++){
