f448a1e9ecd264d4783737eeb1b7cc6727f7ed89
KanonKC <kanonkce@gmail.com>
2022-01-30 17:59:56 +0700

    ผ่านแบบงงๆ

--- Week 2 - Linked List/necklace.cpp
+++ Week 2 - Linked List/necklace.cpp
@@ -3,67 +3,38 @@
 #include <vector>
 using namespace std;
 
+int N,r,d,hp;
 vector<list<int>> necklace;
 vector<int> head;
-vector<int> inFront;
 
-void printList(vector<list<int>> List){
-    cout << "----------\n";
-    for(int i=0;i<List.size();i++){
-        cout << i << ": ";
-        for(auto j:List[i]){
-            cout << j << " -> ";
-        }
-        cout << "\n";
-    }
-}
-
-void printVector(vector<int> v){
-    for(int i=0;i<v.size();i++){
-        cout << v[i] << " ";
-    }
-    cout << "\n";
-}
-
-int getHead(int self){
-    if(self==head[self]){
-        return self;
-    }
-    int rtn = getHead(head[self]);
-    head[self] = rtn;
-    return rtn;
+int getHead(int start){
+    if(head[start] == start) return start;
+    return getHead(head[start]);
 }
 
 int main(){
-    int N,inserter,inserted,hp,recent;
-
     cin >> N;
-
     necklace.resize(N);
-
+    
     for(int i=0;i<N;i++){
-        necklace[i].push_back(i+1);
+        necklace[i].push_back(i);
         head.push_back(i);
-        inFront.push_back(-1);
     }
-
+    
     for(int i=0;i<N-1;i++){
-        cin >> inserter >> inserted;
-        inserter--;
-        inserted--;
-        hp = getHead(inserted);
+        cin >> r >> d;
+        r--;d--;
+        hp = getHead(d);
         for(auto j=necklace[hp].begin();j!=necklace[hp].end();++j){
-            if (*j == inserted+1){
-                necklace[hp].splice(++j,necklace[inserter]);
+            if(*j == d+1){
+                necklace[hp].splice(j,necklace[r]);
+                head[r] = d;
                 break;
             }
         }
-        head[inserter] = head[inserted];
     }
 
     for(auto i:necklace[hp]){
         cout << i << " ";
     }
-    return 0;
-
 }
