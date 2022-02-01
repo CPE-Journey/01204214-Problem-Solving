377407e24a7ca86b7eeda020054b71a8e1031faa
KanonKC <kanonkce@gmail.com>
2022-02-01 21:25:19 +0700

    Very Hard

--- Week 5 - Graph Traversal & Greedy Algorithm/uva10020_minimalcoverage.cpp
+++ Week 5 - Graph Traversal & Greedy Algorithm/uva10020_minimalcoverage.cpp
@@ -3,39 +3,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N,M,L=0,R=0,min_range=999999,dt;
-vector<pair<int,int>> work;
-vector<int> answer;
+int N,M,L,R,dt,current,d1,d2;
 
-int main(){
+void minimalCoverage(){
+    vector<pair<int,int>> work,answer;
     cin >> M;
+
     while(true){
         cin >> L >> R;
-        if(!L && !R){
+        if(L==0 && R==0){
             break;
         }
-
         if(R <= 0 || L >= M) continue;
-
-
+        d1 = L;
+        d2 = R;
         if(L < 0){
-            L = 0;
+            d1 = 0;
         }
         if(R > M){
-            R = M;
+            d2 = M;
         }
-        work.push_back(make_pair(R,L));
-        
+
+        work.push_back(make_pair(L,R));
     }
 
     sort(work.begin(),work.end());
 
-    for(int i=0;i<work.size();i++){
-        if
-    }
+    current = 0;
+    pair<int,int> select;
+    select.first = 0;
+    select.second = 0;
 
-    // for(int i=0;i<work.size();i++){
-    //     cout << work[i].first << " " << work[i].second.first << " " << work[i].second.second << "\n";
-    // }
-    // cout << answer[0] << " " << answer[1];
+    int i=0;
+    while(current < M && i < work.size()){
+        select.first = 0;
+        select.second = 0;
+        while(work[i].first <= current){
+            if(work[i].second > select.second){
+                select.first = work[i].first;
+                select.second = work[i].second;
+            }
+            i++;
+        }
+        if(select.second == current) break;
+        current = select.second;
+        answer.push_back(make_pair(select.first,select.second));
+    }
+    if(current >= M){
+        cout << answer.size() << "\n";
+        for(int i=0;i<answer.size();i++){
+            cout << answer[i].first << " " << answer[i].second << "\n";
+        }
+    }
+    else{
+        cout << "0\n";
+    }
+}
+
+int main(){
+    cin >> N;
+    for(int i=0;i<N;i++){
+        minimalCoverage();
+        cout << "\n";
+    }
 }
