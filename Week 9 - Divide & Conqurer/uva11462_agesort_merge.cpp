f357033dd676b5a02fd390a50014a7613540df64
kanonkc <kanonkce@gmail.com>
2022-02-25 17:58:53 +0700

    Pract 2

--- Week 9 - Divide & Conqurer/uva11462_agesort_merge.cpp
+++ Week 9 - Divide & Conqurer/uva11462_agesort_merge.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 using namespace std;
 
-<<<<<<< HEAD
 int N,input;
 vector<int> age;
 
@@ -69,34 +68,4 @@ int main(){
         age.clear();
     }
     return 0;
-=======
-vector<int> age = {3,4,2,1,5};
-
-void merge(int s,int t){
-    int start=s+0,mid=s+(t-s)/2;
-
-    while(start <= s+(t-s)/2 && mid <= t){
-        if(age[start] < age[mid]){
-            start++;
-        }
-        else{
-            swap(age[start],age[mid]);
-            mid++;
-        }
-    }
-}
-
-void mergeSort(int s,int t){
-    printf("%d %d\n",s,t);
-    if(s == t){
-        return;
-    }
-    mergeSort(s,s+(t-s)/2);
-    mergeSort(s+(t-s)/2+1,t);
-    merge(s,t);
-}
-
-int main(){
-    mergeSort(0,4);
->>>>>>> f37615646f994da39f4f7457b9f7e6c109361dde
 }
