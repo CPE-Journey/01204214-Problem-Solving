4a6e77444551dc7f6fbfe7c0f414dbec577513c2
KanonKC <kanonkce@gmail.com>
2022-03-13 02:32:42 +0700

    clear shell

---
+++ Week 10 - Dynamic Programming/inc2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N,input;
+vector<vector<int>> sequence;
+
+int main(){
+    cin >> N;
+    for(int i=0;i<N;i++){
+        cin >> input;
+        
+    }
+}
---
+++ clearexe.sh
@@ -0,0 +1,2 @@
+rm -r */*.exe
+echo "All .exe file(s) have been removed!"
