ca9fe27f954173105455b7d39d3f4a862d1c0e5a
KanonKC <kanonkce@gmail.com>
2022-04-11 00:11:41 +0700

    5

--- Week 08 - MST & Shortest Path/shortestpath.cpp
+++ Week 08 - MST & Shortest Path/shortestpath.cpp
@@ -7,21 +7,6 @@ vector<int> shortest,visited;
 vector<vector<pair<int,int>>> graph; // Connect Edge, Weight
 priority_queue<pair<int,int>> heap;  // Shorest, Node
 
-int Max(int a,int b){
-    return a > b ? a : b;
-}
-
-int Min(int a,int b){
-    return a > b ? b : a;
-}
-
-void printShortest(){
-    for(int i=0;i<N;i++){
-        cout << shortest[i] << " ";
-    }
-    cout << "\n";
-}
-
 int main(){
     cin >> N >> M;
     graph.resize(N);
@@ -30,8 +15,8 @@ int main(){
     heap.push(make_pair(0,0));
 
     for(int i=1;i<N;i++){
-        shortest.push_back(1001);
-        heap.push(make_pair(-1001,i));
+        shortest.push_back(999999);
+        heap.push(make_pair(-999999,i));
     }
 
     for(int i=0;i<M;i++){
@@ -43,15 +28,11 @@ int main(){
     int s_node,s_dist;
     pair<int,int> select;
     remaining = N;
-    while(remaining){
+    while(!heap.empty()){
         select = heap.top();
         heap.pop();
         s_node = select.second;
         s_dist = -select.first;
-
-        if(visited[s_node]) continue;
-        visited[s_node] = 1;
-        remaining--;
 
         for(int j=0;j<graph[s_node].size();j++){
             
@@ -59,11 +40,12 @@ int main(){
             current_distance = shortest[current_node];
             new_distance = s_dist+graph[s_node][j].second;
 
-            if(!visited[current_node]){
-                shortest[current_node] = Min(current_distance,new_distance);
-                heap.push(make_pair(Max(-current_distance,-new_distance),current_node));
+            if(new_distance < current_distance){
+                shortest[current_node] = new_distance;
+                heap.push(make_pair(-new_distance,current_node));
             }
         }
     }
     cout << shortest[N-1];
+    return 0;
 }
--- Week 12 - Final Review/fin61_spacesignals.cpp
+++ Week 12 - Final Review/fin61_spacesignals.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int current = 0,count = 0;
-string s,text;
+int current = 0,count = 0,len;
+string name,message;
 
 int main(){
-    cin >> s >> text;
-    for(int i=0;i<text.length();i++){
-        
+    cin >> name >> message;
+    len = name.length();
+    for(int i=0;i<message.length();i++){
+        if(message[i] == name[current]){
+            count += (current+1)/len;
+            current = (current+1)%len;
+        }
     }
+    cout << count;
+    return 0;
 }
