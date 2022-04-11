1f634dcc9f3ce62c0a17a1d07bd6c674f2bf5aa5
KanonKC <kanonkce@gmail.com>
2022-04-11 17:55:16 +0700

    Spread + Doctor

--- Week 12 - Final Review/fin61_spread.cpp
+++ Week 12 - Final Review/fin61_spread.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int N,M;
+int N,M,u,v,input,h1,h2;
 vector<int> head,depth,speed;
+vector<pair<int,pair<int,int>>> edge;
 
 int Ceil(int n,int d){
     int rem = n%d;
-    cout << rem << "\n";
     int res = n/d;
-    cout << res << "\n";
     if(rem != 0){
         return res+1;
     }
@@ -35,7 +34,43 @@ void unionHead(int a,int b){
     }
 }
 
+int Max(int a,int b){
+    return a > b ? a : b;
+}
+
 int main(){
-    cout << "Hello";
-    cout << Ceil(5,2);
+    cin >> N >> M;
+    for(int i=0;i<N;i++){
+        cin >> input;
+        head.push_back(i);
+        depth.push_back(0);
+        speed.push_back(input);
+    }
+
+    for(int i=0;i<M;i++){
+        cin >> u >> v >> input;
+        edge.push_back(make_pair(Ceil(input,speed[u-1]+speed[v-1]),make_pair(u-1,v-1)));
+    }
+
+    sort(edge.begin(),edge.end());
+
+    int maxTime = 0,i=0,unionCount = 0;
+    while(unionCount < N-1){
+        h1 = findHead(edge[i].second.first);
+        h2 = findHead(edge[i].second.second);
+        
+        if(h1 != h2){
+            unionHead(h1,h2);
+            unionCount += 1;
+            maxTime = Max(maxTime,edge[i].first);
+        }
+        i++;
+    }
+
+    cout << maxTime << "\n";
+
+    // for(int i=0;i<M;i++){
+    //     printf("%d %d %d\n",edge[i].second.first,edge[i].second.second,edge[i].first);
+    // }
+
 }
--- Week 12 - Final Review/fin62_doctors.cpp
+++ Week 12 - Final Review/fin62_doctors.cpp
@@ -2,21 +2,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N,M,v1,v2,w,current_distance,new_distance,current_node;
-vector<int> shortest,visited;
+int N,M,L,v1,v2,w,current_distance,new_distance,current_node,minCost=0;
+vector<int> shortest,population;
 vector<vector<pair<int,int>>> graph; // Connect Edge, Weight
 priority_queue<pair<int,int>> heap;  // Shorest, Node
 
+int Min(int a,int b){
+    return a > b ? b : a;
+}
+
 int main(){
-    cin >> N >> M;
+    cin >> N >> M >> L;
     graph.resize(N);
-    visited.resize(N);
     shortest.push_back(0);
     heap.push(make_pair(0,0));
 
     for(int i=1;i<N;i++){
         shortest.push_back(999999);
         heap.push(make_pair(-999999,i));
+    }
+
+    for(int i=0;i<N;i++){
+        cin >> w;
+        population.push_back(w);
     }
 
     for(int i=0;i<M;i++){
@@ -45,6 +53,9 @@ int main(){
             }
         }
     }
-    cout << shortest[N-1];
+    for(int i=1;i<N;i++){
+        minCost += shortest[i]*Min(L,population[i]);
+    }
+    cout << minCost << "\n";
     return 0;
 }
